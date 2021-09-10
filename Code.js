fbdd0fc5d9e15b60e805f32194e5f835549dc1a0
Maulik-afk <78996049+Maulik-afk@users.noreply.github.com>
2021-09-10 16:47:34 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+
+fill("white")
+textSize(15)
+text("CUTNIFON", 600,100)
+fill("lightblue")
+text("Hint: performs a particular task!!", 600,150)
+
+
+fill("white")
+textSize(15)
+text("ATADASBA", 100,350)
+fill("lightblue")
+text("Hint: stores all information!!", 100,400)
+
+}
